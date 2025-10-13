256cb8c15f4460ee7a7b1a3dfa3d6545ef694181
joshcullen <joshcullen10@gmail.com>
2025-10-13 17:01:05 -0400

    Updated MATLAB code based on Gemini suggestion per OPeNDAP compatability w URL

--- Complex_GHAs/src/download_export_sst.m
+++ Complex_GHAs/src/download_export_sst.m
@@ -13,14 +13,16 @@ get_date = char(yesterday_date); % 'YYYY-MM-DD' format
 %%% Read in data from coastal Mid-Atlantic Bight
 
 % Define url
-url_template = 'https://coastwatch.pfeg.noaa.gov/erddap/griddap/jplMURSST41.nc?analysed_sst[%sT09:00:00Z]:1:[%sT09:00:00Z][(35.0):1:(40.0)][(-78.0):1:(-74.0)]';
-final_url = sprintf(url_template, get_date, get_date);
+url_template = 'https://coastwatch.pfeg.noaa.gov/erddap/griddap/jplMURSST41.nc?analysed_sst[(%sT09:00:00Z)][(35.0):(40.0)][(-78.0):(-74.0)]';
+final_url = sprintf(url_template, get_date);
+
 
 % Read the data from the downloaded NetCDF file
+% MATLAB ncread is OPeNDAP-compatible and will handle the download and slicing
 sst_data = ncread(final_url, 'analysed_sst');
 lat = ncread(final_url, 'latitude');
 lon = ncread(final_url, 'longitude');
 
-% Check that code is working
+% Check that code is working (also fixed the fprintf syntax)
 sst_vector = sst_data(:);
-fprintf('Mean is', mean(sst_vector));
+fprintf('Mean is %.4f\n', mean(sst_vector));
